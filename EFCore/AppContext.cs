fa0f2fa9b4fb79003135d4957383987c317852ef
Vitali Nikolenko <nvvnikolenko@gmail.com>
2020-02-10 16:37:38 +0200

    Add project files.

---
+++ EFCore/AppContext.cs
@@ -0,0 +1,20 @@
+﻿namespace EFCore
+{
+    using EFCore.Entities;
+    using Microsoft.EntityFrameworkCore;
+
+    public class AppContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public AppContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=helloappdb;Trusted_Connection=True;");
+        }
+    }
+}
